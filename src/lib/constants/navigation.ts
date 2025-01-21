d152f88620425cf445038bea551d4649fdf8cf5a
nikkiarae <nikkiandrewrae@gmail.com>
2025-01-21 11:06:40 +0000

    Setup

---
+++ src/lib/constants/navigation.ts
@@ -0,0 +1,1 @@
+export const FOOTER_LINKS = [  { name: "Privacy Policy", href: "/privacy" },  { name: "Terms of Service", href: "/terms" },];export const SOCIAL_LINKS = [  { name: "GitHub", href: "https://github.com", icon: HomeIcon },  { name: "Twitter", href: "https://twitter.com", icon: DocumentTextIcon },  { name: "LinkedIn", href: "https://linkedin.com", icon: PhoneIcon },];
